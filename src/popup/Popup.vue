d5bb570611109536719e8196d5919a6c8ebd989a
zhang771 <19939926438@163.com>
2024-03-13 09:28:51 +0800

    chore(clear): clear some files

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -8,10 +8,6 @@ function openOptionsPage() {
 
 <template>
   <main class="w-[300px] px-4 py-5 text-center text-gray-700">
-    <Logo />
-    <div>Popup</div>
-    <SharedSubtitle />
-
     <button class="btn mt-2" @click="openOptionsPage">
       Open Options
     </button>
